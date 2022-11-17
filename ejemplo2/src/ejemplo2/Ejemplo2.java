db80524fe594bfdedc0ec36922ea5b360f281c28
franc <franc@LAPTOP-UD2FD6E6>
2022-11-16 22:47:32 -0600

    hola

--- ejemplo2/src/ejemplo2/Ejemplo2.java
+++ ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -15,6 +15,7 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         System.out.println("hola prros");
+        System.out.println("dame dinero");
     }
     
 }
