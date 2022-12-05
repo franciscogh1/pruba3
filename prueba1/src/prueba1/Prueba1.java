9f43ba84e491f8f3a46b94240c7acbdfad7d7eeb
franc <franc@LAPTOP-UD2FD6E6>
2022-12-05 15:35:30 -0600

    hola

--- prueba1/src/prueba1/Prueba1.java
+++ prueba1/src/prueba1/Prueba1.java
@@ -14,7 +14,7 @@ public class Prueba1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic hera
+        System.out.println("holamundo");
     }
     
 }
